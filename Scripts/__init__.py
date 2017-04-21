7f7c7f800a51fc5b0f42b24cb3bfa8ea72030fcf
JFulford85 <JMFulford85@gmail.com>
2017-04-21 16:19:37 -0500

    Initial script

---
+++ Scripts/__init__.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import mysql.connector
+
+tsharkCall = ['C:\\Program Files\\WireShark\\tshark.exe', '-a', 'duration:100']
+tsharkOut = open('tshark.txt', 'wb')
+
+tsharkProc = subprocess.call(tsharkCall, stdout = tsharkOut)
+
+#Things to add to the program.
+# 1: Inserting into database (in progress, still debugging)
+# 2: Picking a good run time for our tshark data collection. Will need advice on that
+# 3: Utilizing data to model network behavior. Probably R or some matlab sollution.
+# 4: Use mathematical approach to handle blocking of ports. Probably block port 23 (telnet)
